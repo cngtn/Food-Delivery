1f072d0436e8b403a2e2f4e4b1aa36ce20f8d26b
Hoài Bảo <hoaibao572@gmail.com>
2022-06-28 16:08:25 +0700

    Tets

---
+++ lib/utils/colors copy.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  static final Color textColor = const Color(0xFFCCC7C5);
+  static final Color mainColors = const Color(0xFF89DAD0);
+  static final Color iconColors1 = const Color(0xFFFFD28D);
+  static final Color iconColors2 = const Color(0xFFFCAB88);
+  static final Color iconColors3 = const Color(0xFFfb3434);
+  static final Color paraColors = const Color(0xFF8F837F);
+  static final Color buttonBackgroundColors = const Color(0xFFF7F6F4);
+  static final Color signColors = const Color(0xFFA9A29F);
+  static final Color titleColors = const Color(0xFF5C524F);
+  static final Color mainBlackColors = const Color(0xFF332D2b);
+  static final Color yellowColors = const Color(0xFFFFD379);
+}
