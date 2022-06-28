ab2797ed0b1887e68e82fbeaba5b0b94d403ba5f
Hoài Bảo <hoaibao572@gmail.com>
2022-06-28 16:06:50 +0700

    Test

---
+++ lib/utils/dimensions copy.dart
@@ -0,0 +1,54 @@
+import 'package:get/get.dart';
+
+class Dimensions {
+  static double screenHeight = Get.context!.height;
+  static double screenWidth = Get.context!.width;
+
+  static double pageView = screenHeight / 2.64;
+  static double pageViewContainer = screenHeight / 3.84;
+  static double pageViewTextContainer = screenHeight / 7.03;
+
+//Dynamic height
+  static double height10 = screenHeight / 84.4;
+  static double height15 = screenHeight / 56.27;
+  static double height20 = screenHeight / 42.2;
+  static double height30 = screenHeight / 28.13;
+  static double height40 = screenHeight / 22.1;
+  static double height45 = screenHeight / 18.76;
+  static double height50 = screenHeight / 16.88;
+
+//Dynamic width
+  static double width10 = screenHeight / 84.4;
+  static double width15 = screenHeight / 56.27;
+  static double width20 = screenHeight / 42.2;
+  static double width30 = screenHeight / 28.13;
+  static double width40 = screenHeight / 22.1;
+  static double width45 = screenHeight / 18.76;
+  static double width50 = screenHeight / 16.88;
+
+  //font size
+  static double fontSize16 = screenHeight / 52.75;
+  static double font20 = screenHeight / 42.2;
+  static double font26 = screenHeight / 32.4;
+  //radius
+  static double radius15 = screenHeight / 56.27;
+  static double radius10 = screenHeight / 84.4;
+  static double radius20 = screenHeight / 42.2;
+  static double radius30 = screenHeight / 32.28;
+
+  //icon size
+  static double icon24 = screenHeight / 35.17;
+  static double icon20 = screenHeight / 42.2;
+  static double icon16 = screenHeight / 52.75;
+  static double icon36 = screenHeight / 23.44;
+
+  //List view size
+  static double listViewImg = screenWidth / 3.60;
+  static double listViewTextCount = screenWidth / 3.9;
+
+  //popular food detail
+  static double popularFoodImgSize = screenHeight / 2.41;
+
+  //bottom height
+  static double bottomHeight = screenHeight / 11;
+}
